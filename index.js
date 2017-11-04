cbe5f53e3e0114b37d91becae3997e6dee9ecda8
Ryan Do <falcsmike7@gmail.com>
2017-11-04 12:09:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // Code your solution in this file.
-function lowerCaseDrivers(drivers) {
-  return drivers.map(function(driver) {
-    return driver.toLowerCase();
-  })
+function lowerCaseDrivers(array) {
+  return array.map(function(element) {
+    return element.toLowerCase();
+  });
+}
+
+function nameToAttributes(array) {
+  return array.map(function(element) {
+    const name = element.split(" ");
+    let elementFirst = name[0];
+    let elementLast = name[1];
+
+    return { firstName: elementFirst, lastName: elementLast };
+  });
 }
